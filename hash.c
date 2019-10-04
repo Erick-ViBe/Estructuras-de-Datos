a72385a0b741ad5815ac50028699484c5fac9f9d
Erick Vicencio Benitez <erickvb12@gmail.com>
2019-10-04 18:03:32 -0500

    Actualizacion

--- hash.c
+++ hash.c
@@ -4,12 +4,30 @@
 int 
 main(void){
 
-	int tamaño_hash = 100;
-	int hash[tamaño_hash];
+	int tamano_hash = 25;
+	int hash[tamano_hash];
+	int aux;
+	int pos;
 
+	for (int i = 0; i < tamano_hash; ++i){
 
+		hash[i] = 0;
+	}
+
+	printf("---------------\n");
+
+	scanf("%d", &aux);
+
+	printf("---------------\n");
+
+	pos = ((aux*124)%tamano_hash)-1;
+
+	hash[pos] = aux;
+
+	for (int i = 0; i < tamano_hash; ++i){
+		
+		printf("%d\n", hash[i]);
+	}
 
 	return 0;
 }
-
-// h(k) = (k*m)%m;
