b85e203ac184c6eabe447213a924a4bf201781f2
Erick Vicencio Benitez <erickvb12@gmail.com>
2019-11-14 00:54:16 -0600

    Actualizacion

--- arbolavl.c
+++ arbolavl.c
@@ -8,32 +8,51 @@ struct Nodo{
 	struct Nodo *hijoIzquierdo;
 };
 
-void insertar();
+struct Nodo* insertar();
 
 int main(void) {
 
    struct Nodo *raiz = NULL;
 
-	insertar(&raiz);
+	raiz = insertar(raiz);
+	raiz = insertar(raiz);
 
 	printf("%d\n", raiz->dato);
 
-   return 0;
+	return 0;
 }
 
-void insertar(struct Nodo **raizArbol) {
+struct Nodo*
+insertar(struct Nodo *raizArbol) {
 
    int datoRecibido;
 
-   if (*raizArbol == NULL) {
-      *raizArbol = malloc(sizeof(struct Nodo));
+   if (raizArbol == NULL) {
+
+      raizArbol = malloc(sizeof(struct Nodo));
       scanf("%d", &datoRecibido);
-      (*raizArbol)->dato = datoRecibido;
-      return;
+      raizArbol->dato = datoRecibido;/* code */
+		raizArbol->hijoDerecho = NULL;
+		raizArbol->hijoIzquierdo = NULL;
+		return raizArbol;
    }else{
 
-		// struct Nodo *nuevo = malloc(sizeof(struct Nodo));
-		printf("ajhsouaos\n");
+		struct Nodo *nuevo = malloc(sizeof(struct Nodo));
+		if (nuevo = NULL) {
+			return NULL;
+		}
+		nuevo->hijoDerecho = NULL;
+		nuevo->hijoIzquierdo = NULL;
+		scanf("%d", &datoRecibido);
+		if (raizArbol->dato == datoRecibido) {
 
+			return raizArbol;
+		} else if (datoRecibido > raizArbol->dato) {
+
+			raizArbol->hijoDerecho = insertar(raizArbol->hijoDerecho);
+		} else if (datoRecibido < raizArbol->dato) {
+;
+			raizArbol->hijoIzquierdo = insertar(raizArbol->hijoIzquierdo);
+		};
    }
 }
